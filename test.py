ce0150390bab7391b485fd29d8e580fb656a89e4
lxxcuc2021 <78137626+lxxcuc2021@users.noreply.github.com>
2021-09-02 14:33:16 +0800

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-import time
-
-driver = webdriver.Chrome()
-driver.maximize_window()
-driver.get('https://www.sportybet.com/ng/')
-time.sleep(10)
-driver.find_element_by_link_text('/ng/sport/football').click()
-time.sleep(10)
